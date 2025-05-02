4dd9fa726869debbb72bb704fdf16f06c09358e9
kss418 <jis1595@naver.com>
2025-05-02 15:14:56 +0900

    db 테스트 코드 작성

--- server/main.cpp
+++ server/main.cpp
@@ -2,5 +2,18 @@
 #include <mariadb/conncpp.hpp>
 
 int main(){
-    
+    sql::Driver* driver = sql::mariadb::get_driver_instance();
+
+    // 환경 변수 설정하기
+    std::string pw = getenv("DB_PASSWORD");
+
+    sql::Properties props({
+        {"host", "localhost"},
+        {"port", "3306"},
+        {"user", "root"},
+        {"password", pw},
+        {"schema", "test"} 
+    });
+
+    return 0;
 }
