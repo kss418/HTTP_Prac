b0f31f0b3ffa9a6b559e42a67795d8c1daf06e23
kss418 <jis1595@naver.com>
2025-05-14 17:36:08 +0900

    http 클라이언트 예제 작성

--- client/main.cpp
+++ client/main.cpp
@@ -1,15 +1,32 @@
 #include <iostream>
 #include <filesystem>
-#include <vector>
 #include <string>
+#include <boost/asio.hpp>
+#include <boost/beast.hpp>
+#include <boost/beast/http.hpp>
 #include "include/CmdHelper.hpp"
 #include "include/FsHelper.hpp"
 
+namespace http = boost::beast::http;
+
 int main(){
-    auto cwd = std::filesystem::current_path().root_directory();
     CmdHelper cmd_helper;
+    const std::string ip = "127.0.0.1";
+    const unsigned short port = 80;
+    const int version = 11;
+
+    boost::asio::io_context io_context;
+    boost::beast::tcp_stream stream(io_context);
+    boost::asio::ip::tcp::endpoint endpoint = {boost::asio::ip::make_address(ip), port};
+    stream.connect(endpoint);
+
+    http::request <http::string_body> req{
+        http::verb::get, "login", version
+    };
+    req.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
+    http::write(stream, req);
+
     while(1){
-        cmd_helper.get_cmd();
-        
+        //cmd_helper.get_cmd();
     }
 }
