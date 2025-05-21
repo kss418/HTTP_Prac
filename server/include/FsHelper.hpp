f62077c7fefa99a26d55e30957d79333ff556a0f
kss418 <jis1595@naver.com>
2025-05-21 15:03:22 +0900

    서버 측 파일시스템 코드 일부 작성

---
+++ server/include/FsHelper.hpp
@@ -0,0 +1,33 @@
+#pragma once
+#include <memory>
+#include <mutex>
+#include <filesystem>
+#include <unordered_map>
+
+class FsExecuter{
+public:
+    FsExecuter(const std::string& id);
+    std::filesystem::path cwd() const;
+    bool exists(const std::filesystem::path& cwd) const;
+    void set_cwd(const std::filesystem::path& cwd);
+    void mkdir(const std::filesystem::path& cwd);
+    void rmdir(const std::filesystem::path& cwd);
+    void ls() const;
+private:
+    std::filesystem::path m_working_path;
+};
+
+class FsHelper{
+public:
+    static FsHelper& get_instance();
+
+    FsHelper() = default;
+    FsHelper(const FsHelper&) = delete;
+    FsHelper& operator=(const FsHelper&) = delete;
+    FsHelper(FsHelper&&) = delete;
+    FsHelper& operator=(FsHelper&&) = delete;
+private:
+    static std::once_flag m_init_flag;
+    static std::unique_ptr<FsHelper> m_instance;
+    std::unordered_map<std::string, std::unique_ptr<FsExecuter>> m_map;
+};
---
+++ server/src/FsHelper.cpp
@@ -0,0 +1,50 @@
+#include "../include/FsHelper.hpp"
+
+std::once_flag FsHelper::m_init_flag;
+std::unique_ptr<FsHelper> FsHelper::m_instance;
+
+FsHelper& FsHelper::get_instance(){
+    std::call_once(m_init_flag, [](){
+        m_instance = std::make_unique<FsHelper>();
+    });
+}
+
+FsExecuter::FsExecuter(const std::string& id)
+  :  m_working_path(std::filesystem::current_path().root_directory() / id){  
+}
+
+std::filesystem::path FsExecuter::cwd() const{
+    return m_working_path.string();
+}
+
+bool FsExecuter::exists(const std::filesystem::path& cwd) const{
+    return std::filesystem::exists(cwd);
+}
+
+void FsExecuter::set_cwd(const std::filesystem::path& cwd){
+    m_working_path = cwd;
+};
+
+void FsExecuter::mkdir(const std::filesystem::path& cwd){
+    const auto path = cwd.is_absolute() ? cwd : (m_working_path / cwd);
+    if(!std::filesystem::create_directories(path)){
+        // std::cout << "이미 존재하는 디렉토리입니다." << std::endl;
+    }
+};
+
+void FsExecuter::rmdir(const std::filesystem::path& cwd){
+    const auto path = cwd.is_absolute() ? cwd : (m_working_path / cwd);
+    if(!std::filesystem::exists(path)){
+        // std::cout << "대상 디렉토리가 존재하지 않습니다." << std::endl;
+    }
+    else if(!std::filesystem::is_directory(path)){
+        // std::cout << "대상이 디렉토리가 아닙니다." << std::endl;
+    }
+    else if(!std::filesystem::remove_all(path)){
+        // std::cout << "빈 디렉토리가 아닙니다." << std::endl;
+    }
+};
+
+void FsExecuter::ls() const{
+
+};
