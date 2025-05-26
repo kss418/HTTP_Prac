c92fc6ca6947116e43d6450b1098e905704edb32
kss418 <jis1595@naver.com>
2025-05-26 22:07:05 +0900

    환경변수 검증코드 추가

--- server/main.cpp
+++ server/main.cpp
@@ -8,8 +8,21 @@
 using tcp = boost::asio::ip::tcp;
 namespace http = boost::beast::http;
 
+void chk(){
+    const char* salt_env = std::getenv("SALT");
+    if(!salt_env){
+        std::cout << "SALT 환경변수 미설정" << std::endl;
+    }
+
+    const char* db_pw = std::getenv("DB_PASSWORD");
+    if(!db_pw){
+        std::cout << "DB_PASSWORD 환경변수 미설정" << std::endl;
+    }
+}
+
 int main(){
     boost::asio::io_context io_context;
+    chk();
     Acceptor acceptor(io_context, 8080);
     io_context.run();
 
